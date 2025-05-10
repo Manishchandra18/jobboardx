359289308e0836ed38472704900ef626585dc30f
manish <manish.guturu@hiringhood.com>
2025-05-10 13:46:45 +0530

    Final

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+import { connect } from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await connect(process.env.MONGO_URI);
     console.log('MongoDB connected ✨');
   } catch (err) {
     console.error('❌ DB connection error:', err.message);
@@ -10,4 +10,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
